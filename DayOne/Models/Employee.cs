75c5b891bcac9cde5f90142e8cc8703176037076
khayalgeek <xeyalbinnetov02@gmail.com>
2024-12-20 16:57:41 +0400

    init

---
+++ DayOne/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DayOne.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public ICollection<EmployeeContact> EmployeeContacts { get; set; } 
+
+        public void GetEmployeeContacts()
+        {
+            foreach(var employeContact in EmployeeContacts)
+            {
+                Console.WriteLine(employeContact.Address, employeContact.Phone);
+            }
+        }
+    }
+}
+
