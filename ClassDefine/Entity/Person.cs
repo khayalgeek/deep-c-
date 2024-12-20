23dddddda510a7203e4ca42f9b9b454213c6c614
khayalgeek <xeyalbinnetov02@gmail.com>
2024-12-20 17:37:27 +0400

    class define

---
+++ ClassDefine/Entity/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ClassDefine.Entity
+{
+	public class Person
+	{
+		public string Name { get; set; }
+        public string Surname { get; set; }
+		public string Nationality { get; set; }
+    }
+}
+
